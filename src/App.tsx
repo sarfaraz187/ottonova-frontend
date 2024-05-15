0e89fee60449d0df5c043f60e429d108c53ddb5f
sarfaraz187 <sarfarazmohammed187@gmail.com>
2024-05-15 18:36:52 +0200

    issue fix in shadcn components json file

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export const App = () => {
   return (
     <>
       <h1 className="text-3xl underline">Hello world!</h1>
-      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
       <Home />
     </>
   );
