9418a6a180a0742eae87f976e92337b058dab478
sarfaraz187 <sarfarazmohammed187@gmail.com>
2024-05-15 18:10:43 +0200

    fixed issue with shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
-import { Home } from './pages/Home';
-import { Button } from './components/ui/button';
+import { Home } from '@/pages/Home';
+import { Button } from '@/components/ui/button';
 
 export const App = () => {
   return (
     <>
       <h1 className="text-3xl underline">Hello world!</h1>
-      <Button />
+      <Button variant="secondary">Secondary</Button>
       <Home />
     </>
   );
