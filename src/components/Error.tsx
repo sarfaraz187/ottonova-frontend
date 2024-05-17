0330adef76d63c57a84838e78cb140a1fecd06c5
sarfaraz187 <sarfarazmohammed187@gmail.com>
2024-05-17 12:53:13 +0200

    New Error page created

---
+++ src/components/Error.tsx
@@ -0,0 +1,7 @@
+const Error = () => {
+  return (
+    <div className="max-w-7xl m-auto p-6 flex justify-center">Error Page</div>
+  );
+};
+
+export default Error;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,32 +3,36 @@ import axiosClient from '@/services/http';
 import { useEffect, useState } from 'react';
 import { CityCard } from './CityCard';
 import { useNavigate } from 'react-router-dom';
+import Error from '@/components/Error';
 
 export const Home = () => {
   const httpClient = axiosClient();
-  const [cities, setCities] = useState<City[]>();
+  const [cities, setCities] = useState<City[]>([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     (async () => {
       const response = await httpClient.get('cities');
       console.log('Cities from backend :', response);
-      if (response.data) setCities(response.data);
+      setCities(response.data.length > 0 ? response.data : []);
     })();
   }, []);
 
   return (
     <main className="bg-slate-200 z-40 mt-20 h-vh">
-      <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 pt-6 gap-4 max-w-7xl m-auto p-6 h-vh">
-        {cities?.map((city) => {
-          return (
-            <CityCard
-              city={city}
-              onClick={() => navigate(`/cities/${city.id}`)}
-            />
-          );
-        })}
-      </div>
+      {cities.length > 0 && (
+        <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 pt-6 gap-4 max-w-7xl m-auto p-6 h-vh">
+          {cities?.map((city) => {
+            return (
+              <CityCard
+                city={city}
+                onClick={() => navigate(`/cities/${city.id}`)}
+              />
+            );
+          })}
+        </div>
+      )}
+      {cities.length === 0 && <Error />}
     </main>
   );
 };
