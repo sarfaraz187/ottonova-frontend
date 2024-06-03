168f51272c64904add9f4d58050aae337c7722b3
sarfaraz187 <sarfarazmohammed187@gmail.com>
2024-06-03 16:41:51 +0200

    response payload updated

--- src/api/cities.ts
+++ src/api/cities.ts
@@ -1,14 +1,13 @@
-import { City } from '@/models/City';
 import axiosClient from '@/services/http';
 
 const httpClient = axiosClient();
 
 export async function getCities() {
   const response = await httpClient.get('cities');
-  return response.data;
+  return response.data.cities;
 }
 
 export async function getCityById(id: string) {
   const response = await httpClient.get(`cities/${id}`);
-  return response.data;
+  return response.data.city;
 }
